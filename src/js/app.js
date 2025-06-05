350a22af1c2c9534ee666283fad8ee1c39c112a2
Caroline <c.jungefalk@gmail.com>
2025-06-05 14:48:52 +0200

    Lägg till skal för funktioner

---
+++ src/js/app.js
@@ -0,0 +1,85 @@
+
+/**
+ * Frontend-del av projektet i DT207G
+ * Av: Caroline Jungefalk
+ */
+
+//händelselyssnare
+window.addEventListener("load", init);
+
+//init
+function init(){
+
+    console.log("Sidan har laddat...")
+};
+
+//Hämta gelato
+async function getGelato() {
+    
+};
+
+//Lägg till gelato
+async function postGelato() {
+    
+};
+
+//Uppdatera gelato
+async function putGelato() {
+    
+};
+
+//Ta bort gelato
+async function deleteGelato() {
+    
+};
+
+//Hämta topping
+async function getTopping() {
+    
+};
+
+//Lägg till topping
+async function postTopping() {
+    
+};
+
+//Ta bort topping
+async function deleteTopping() {
+    
+};
+
+//Hämta drink
+async function getDrink() {
+    
+};
+
+//Lägg till drink
+async function postDrink() {
+    
+};
+
+//Ta bort drink
+async function deleteDrink() {
+    
+};
+
+//Hämta kommentarer
+async function getComments() {
+    
+};
+
+//Lägg till kommentar
+async function postComments() {
+    
+};
+
+//Ta bort kommentar
+async function deleteComments() {
+    
+};
+
+
+
+
+
+
